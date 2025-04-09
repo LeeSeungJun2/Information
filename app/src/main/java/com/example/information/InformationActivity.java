87fd3976a7f4687425def32b3be0914cd40a0780
JS-404-000 <jindate2@gmail.com>
2025-04-09 11:23:08 +0900

    commit2 -- 기본구현 완료

--- app/src/main/java/com/example/information/InformationActivity.java
+++ app/src/main/java/com/example/information/InformationActivity.java
@@ -1,6 +1,69 @@
 package com.example.information;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class InformationActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_information);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sub), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextAge = findViewById(R.id.editTextAge);
+
+        RadioButton radioMale = findViewById(R.id.radioMale);
+        RadioButton radioFemale = findViewById(R.id.radioFemale);
+
+        CheckBox cBInfo = findViewById(R.id.cBInfo);
+        CheckBox cBAI = findViewById(R.id.cBAI);
+        CheckBox cBSecurity = findViewById(R.id.cBSecurity);
+
+        Button btnSend = findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent();
+                it.putExtra("id", id);
+                it.putExtra("name", editTextName.getText().toString());
+                it.putExtra("age", editTextAge.getText().toString());
+                if (radioMale.isChecked())
+                    it.putExtra("gender", "남자");
+                else
+                    it.putExtra("gender", "여자");
+
+                String license = "";
+                if (cBInfo.isChecked())
+                    license += "\n정보처리기사";
+                if (cBAI.isChecked())
+                    license += "\n인공지능데이터전문가";
+                if (cBSecurity.isChecked())
+                    license += "\n정보보안기사";
+                it.putExtra("license", license);
+
+                setResult(RESULT_OK, it);
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/information/MainActivity.java
+++ app/src/main/java/com/example/information/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.information;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvResultLable, tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tvResultLable = findViewById(R.id.tVResultLabel);
+        tvResult = findViewById(R.id.tVResult);
+        EditText editTextID = findViewById(R.id.editTextID);
+        Button btnRequest = findViewById(R.id.btnRequest);
+        Button btnEnd = findViewById(R.id.btnEnd);
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), InformationActivity.class);
+                String id = editTextID.getText().toString();
+                it.putExtra("id", id);
+                startActivityForResult(it, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            tvResultLable.setText("입력\n정보\n출력");
+            String str = "아이디: " + data.getStringExtra("id");
+            str = str + "\n이름: " + data.getStringExtra("name");
+            str = str + "\n나이: " + data.getStringExtra("age");
+            str = str + "\n성별: " + data.getStringExtra("gender");
+            str = str + "\n자격증: " + data.getStringExtra("license");
+            tvResult.setText(str);
+        }
     }
     // commit 1
 }
